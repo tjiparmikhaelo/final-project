2ef814d22c0660da8e5ddc2461650e3dd29c0912
theov <theov7406@gmail.com>
2022-07-25 12:13:14 +0700

    reset database

--- src/main/java/com/app/SecondGadgetApp/Repository/ProductsRepo.java
+++ src/main/java/com/app/SecondGadgetApp/Repository/ProductsRepo.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Transactional
 public interface ProductsRepo extends JpaRepository<Products, Long>
 {
-    @Query(value = "select * from products p where not p.product_status = 'archive' and not p.product_status = 'sold' order by created_at desc", nativeQuery = true)
+    @Query(value = "select * from products p where not p.product_status = 'archive' and not p.product_status = 'sold' order by created_at desc limit 8", nativeQuery = true)
     List<Products> ProductDesc();
     @Query(value = "select * from products p where p.product_name like concat ('%',:productName,'%') and not p.product_status = 'archive' and not p.product_status = 'sold' order by p.created_at desc", nativeQuery = true)
     List<Products> findByProductName(String productName);
@@ -23,7 +23,7 @@ public interface ProductsRepo extends JpaRepository<Products, Long>
     @Query(value = "select * from products p join users u on p.user_id = u.user_id where username = ?1 order by p.updated_at DESC", nativeQuery = true)
     List<Products> ProductByUsername(String username);
     @Modifying
-    @Query(value = "select * from products p where category_id = ?1 and not p.product_status = 'archive' and not p.product_status = 'sold' order by created_at desc", nativeQuery = true)
+    @Query(value = "select * from products p where category_id = ?1 and not p.product_status = 'archive' and not p.product_status = 'sold' order by created_at desc limit 4", nativeQuery = true)
     List<Products> ProductByCategory(Long categoryId);
     @Modifying
     @Query(value = "select * from products p join users u on p.user_id = u.user_id where p.product_name like concat ('%',:productName,'%') and p.category_id = :categoryId and u.city_id = :idCity and not p.product_status = 'archive' and not p.product_status = 'sold' order by p.created_at desc", nativeQuery = true)
